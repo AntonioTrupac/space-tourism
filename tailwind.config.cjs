d8504e3d7b58b4a38781807cb0734d1e102c99e4
AntonioTrupac <sniper.trupac@gmail.com>
2023-02-14 19:56:32 +0100

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				primary: "hsl(230,35%,7%)",
-				secondary:"hsl(231,77%,90%)",
-				tertiary: "hsl(0,0%,100%)",
-				"transparent-nav": "hsl(0 0% 59% / 0.3)",
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(230,35%,7%)",
+        secondary: "hsl(231,77%,90%)",
+        tertiary: "hsl(0,0%,100%)",
+        "transparent-nav": "hsl(0 0% 59% / 0.3)",
+        test: "hsl(0 0% 100% / 0.1)",
+      },
+    },
+  },
+  plugins: [],
+};
